bd9c61f9d679ff467bc745d9030cd6ff85fcbb5c
ethyashpathak <ethyashpathak@gmail.com>
2025-09-06 19:02:21 +0530

    added some utils

--- src/models/issue.model.js
+++ src/models/issue.model.js
@@ -6,19 +6,19 @@ const issueSchema = new mongoose.Schema(
     description: { type: String, required: true },
     location: {
       type: { type: String, enum: ["Point"], default: "Point" },
-      coordinates: { type: [Number], required: true } // [lng, lat]
+      coordinates: { type: [Number], required: true }, // [lng, lat]
     },
     upvotes: { type: Number, default: 0 },
     severity: {
       type: String,
       enum: ["critical", "moderate", "minor"],
-      default: "moderate"
+      default: "moderate",
     },
-    status: { 
-    type: String, 
-    enum: ["pending", "in-progress", "resolved"], 
-    default: "pending" 
-  },
+    status: {
+      type: String,
+      enum: ["pending", "in-progress", "resolved"],
+      default: "pending",
+    },
   },
   { timestamps: true }
 );
